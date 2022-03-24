1ddbeeeec127ef9539e9774a0e97bf5b6802e0bb
PC <168169332@qq.com>
2022-03-24 15:24:33 +0800

    init commit

---
+++ library/src/main/java/cn/cqray/android/util/ResUtils.java
@@ -0,0 +1,25 @@
+package cn.cqray.android.util;
+
+public class ResUtils {
+
+    public static int getIdByName(String name) {
+        return getIdByName(name, "id");
+    }
+
+    public static int getStringIdByName(String name) {
+        return getIdByName(name, "string");
+    }
+
+    public static int getColorIdByName(String name) {
+        return getIdByName(name, "color");
+    }
+
+    public static int getDimenIdByName(String name) {
+        return getIdByName(name, "dimen");
+    }
+
+    public static int getIdByName(String name, String defType) {
+        String packageName = Utils.getContext().getPackageName();
+        return Utils.getResources().getIdentifier(name, defType, packageName);
+    }
+}
---
+++ library/src/main/java/cn/cqray/android/util/Utils.java
@@ -0,0 +1,21 @@
+package cn.cqray.android.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import androidx.annotation.NonNull;
+
+public class Utils {
+
+
+    @NonNull
+    public static Context getContext() {
+
+        return null;
+    }
+
+    @NonNull
+    public static Resources getResources() {
+        return null;
+    }
+}
