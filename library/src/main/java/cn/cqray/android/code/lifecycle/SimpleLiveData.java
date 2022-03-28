6027445e0ac3448e5b8a89bfa7fdcd400ee2db38
PC <168169332@qq.com>
2022-03-28 10:40:27 +0800

    init commit

--- library/src/main/java/cn/cqray/android/code/lifecycle/SimpleLiveData.java
+++ library/src/main/java/cn/cqray/android/code/lifecycle/SimpleLiveData.java
@@ -4,6 +4,12 @@ import android.os.Looper;
 
 import androidx.lifecycle.MutableLiveData;
 
+/**
+ * 简单LiveData
+ * <p>将setValue()、postValue()改为一样的实现。</p>
+ * <p>在主线程中，直接执行。其他线程，跳转线程再执行。</p>
+ * @author Cqray
+ */
 public class SimpleLiveData<T> extends MutableLiveData<T> {
 
     @Override
